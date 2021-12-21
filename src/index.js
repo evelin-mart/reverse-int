ef9d368bedbd17e23aa33eae9e392ff706f4e579
evelin-mart <evalouis666@gmail.com>
2021-12-21 20:09:41 +0300

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = '' + Math.abs(n);
+
+    function reverseString(string) {
+        let i = string.length;
+        let reversed = '';
+        while (i) reversed += string[--i];
+        return reversed;
+    }
+
+    str = reverseString(str);
+
+    return +str;
+
 }
